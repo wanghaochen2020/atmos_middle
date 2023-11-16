36d2bde602861c72e42496cf674a98cbff19af18
whc666 <1095428774@qq.com>
2023-11-16 16:21:22 +0800

    final

--- api/atmosphere.go
+++ api/atmosphere.go
@@ -17,42 +17,9 @@ func Atmosphere(c *gin.Context) {
 	c.String(http.StatusOK, a)
 }
 
-// 获取气象历史数据的请求
-func AtmosphereHistory(c *gin.Context) {
-	facId := c.Query("facId")
-	// 参数中时间格式为:2022-05-05 10:00:00
-	//startTime := trans(c.Query("startTime"))
-	//endTime := trans(c.Query("endTime"))
-	startTime := c.Query("startTime")
-	endTime := c.Query("endTime")
-	b := getHistory(facId, startTime, endTime)
-	c.String(http.StatusOK, b)
-}
-
 func getReal(facId string) string {
 	client := http.Client{}
 	req, err := http.NewRequest(http.MethodGet, url+"?facId="+facId, nil)
-	if err != nil {
-		log.Println("err")
-	}
-	// 添加请求头
-	req.Header.Add("Content-type", "application/x-www-form-url")
-	// 发送请求
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Println("err")
-	}
-	defer resp.Body.Close()
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Println("err")
-	}
-	return string(b)
-}
-
-func getHistory(facId, startTime, endTime string) string {
-	client := http.Client{}
-	req, err := http.NewRequest(http.MethodGet, url+"?facId="+facId+"&startTime="+startTime+"&endTime="+endTime, nil)
 	if err != nil {
 		log.Println("err")
 	}
--- routers/route.go
+++ routers/route.go
@@ -8,5 +8,4 @@ import (
 // 气象站路由
 func AtmosphereDeviceRouter(router *gin.Engine) {
 	router.GET("api/atmosphere", api.Atmosphere)
-	router.GET("api/history", api.AtmosphereHistory)
 }
